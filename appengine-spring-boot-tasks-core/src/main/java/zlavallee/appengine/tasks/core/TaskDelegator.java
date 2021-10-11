916da339059f438ddedbd067b57a85ae4071d189
Zack LaVallee <zlavallee@gmail.com>
2021-10-11 12:06:05 -0500

    Adding logging

--- appengine-spring-boot-tasks-core/src/main/java/zlavallee/appengine/tasks/core/TaskDelegator.java
+++ appengine-spring-boot-tasks-core/src/main/java/zlavallee/appengine/tasks/core/TaskDelegator.java
@@ -4,15 +4,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import zlavallee.appengine.tasks.core.exception.TaskDelegationException;
 
 public class TaskDelegator {
 
+  private static final Logger logger = LoggerFactory.getLogger(TaskDelegator.class);
+
   private final Map<Class<?>, TaskExecutor<?>> taskExecutorMap;
 
   public TaskDelegator(Set<TaskExecutor<?>> taskExecutors) {
+    logger.debug("Registering task executors: {}", taskExecutors);
     taskExecutorMap = taskExecutors.stream()
         .collect(Collectors.toMap(TaskExecutor::getPayloadClass, Function.identity()));
+    taskExecutorMap.forEach(
+        (key, value) -> logger.trace("Registered executor {} for payload class {}", value, key));
   }
 
   public <T> void delegate(T payload) {
